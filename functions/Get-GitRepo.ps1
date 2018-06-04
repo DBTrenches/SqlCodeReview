30777f32a05878f3363641a34e8d6d67a4758433
Peter Vandivier <petervandivier@gmail.com>
2018-06-04 22:16:19 +0100

    Get-GitRepo: add TODO and fix provider-detection

--- functions/Get-GitRepo.ps1
+++ functions/Get-GitRepo.ps1
@@ -1,7 +1,17 @@
 ﻿Function Get-GitRepo {
 <#
-    .SYNOPSIS
-        Return some information about a git repo 
+.SYNOPSIS
+    Return some information about a git repo 
+
+.TODO
+    Add pattern-matching for vsts ssh. Currently assuming following patterns below.
+        https://github.com/$org/$repo.git
+        git@github.com:$org/$repo.git
+        https://$org.visualstudio.com/$project/_git/$repo
+        ssh://$org@vs-ssh.visualstudio.com:$port/$project/_ssh/$repo
+
+.TODO
+    Transform VSTS+SSH remote origin into web-launchable URL.
 #>
     [CmdletBinding()]Param(
         $dir = "."
@@ -17,10 +27,8 @@
         $remoteUrl = ($remoteUrl.Replace("git@github.com:","https://github.com/")) -replace ".{4}$"
     }
 
-    $provider = switch ($remoteUrl.Substring(0,19)) {
-        'https://github.com/' { "github" }
-        'https://viagogo.vis' { "vsts" }
-    }
+    $provider = if($remoteUrl.Contains("github")){ "github" }
+        elseif ($remoteUrl.Contains("visualstudio")){ "vsts" }
 
     [PSCustomObject]@{
         RootFullPath  = (git rev-parse --show-toplevel) 
