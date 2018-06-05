133c440e0a0e09d2513e3320ad22fd91878dafb0
Peter Vandivier <petervandivier@gmail.com>
2018-06-05 07:03:15 +0100

    add remaining functions to module

--- SqlCodeReview.psm1
+++ SqlCodeReview.psm1
@@ -1,8 +1,12 @@
-. $PSScriptRoot\functions\Get-GitRepo.ps1
-. $PSScriptRoot\functions\Get-GitBranch.ps1
 . $PSScriptRoot\functions\Assert-GitBranch.ps1
 . $PSScriptRoot\functions\Enter-GitBranch.ps1
-. $PSScriptRoot\functions\Get-SqlObjectID.ps1
-. $PSScriptRoot\functions\Get-SqlObject.ps1
-. $PSScriptRoot\functions\Get-SqlServerFromConfig.ps1
 . $PSScriptRoot\functions\Format-SqlObjectList.ps1
+. $PSScriptRoot\functions\Get-GitBranch.ps1
+. $PSScriptRoot\functions\Get-GitRepo.ps1
+. $PSScriptRoot\functions\Get-SqlObject.ps1
+. $PSScriptRoot\functions\Get-SqlObjectID.ps1
+. $PSScriptRoot\functions\Get-SqlServerFromConfig.ps1
+. $PSScriptRoot\functions\New-SqlDeployScript.ps1
+. $PSScriptRoot\functions\Request-DBCodeReview.ps1
+. $PSScriptRoot\functions\Write-SqlObjectToLocalPath.ps1
+. $PSScriptRoot\functions\Write-SqlObjListToLocalPath.ps1
