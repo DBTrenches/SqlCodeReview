db3f16230de945c3fd5f88cab86ef5c58a82f3ac
Peter Vandivier <petervandivier@gmail.com>
2018-07-12 13:58:14 +0100

    handle for friendly type mapping

--- SqlCodeReview.variables.ps1
+++ SqlCodeReview.variables.ps1
@@ -4,3 +4,12 @@ $script:sqlCodeReview_DefaultModuleConfig = Get-Content $PSScriptRoot\config\def
 $serverConfigPath = $sqlCodeReview_DefaultModuleConfig.EnvironmentConfigFile
 
 $script:sqlCodeReview_DefaultServerConfig = Get-Content $serverConfigPath | ConvertFrom-Csv
+
+$sqlCodeReview_DefaultModuleConfig.CodeReviewRepo | Get-Member | Where-Object {$_.MemberType -eq "NoteProperty"} | ForEach-Object {
+    $project = $_.name
+    $objectTypeMap = $sqlCodeReview_DefaultModuleConfig.CodeReviewRepo.$project.DirectoryFormat.ObjectTypeMap
+    $objectConfig = Get-Content $objectTypeMap.FilePath -Raw | ConvertFrom-Csv
+
+    $sqlCodeReview_DefaultModuleConfig.CodeReviewRepo.$project.DirectoryFormat.ObjectTypeMap | `
+    Add-Member -MemberType NoteProperty -Name "SubFolders" -Value $objectConfig
+}
